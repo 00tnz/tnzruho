aac2ac6d14c51398f5f3041a9481dc8a214da6d3
bread <gingerwortel.vanoranje@gmail.com>
2025-12-11 09:06:18 +0100

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -1,1 +1,1 @@
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/refs/heads/main/Library.lua"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/refs/heads/main/Library.lua"))()
